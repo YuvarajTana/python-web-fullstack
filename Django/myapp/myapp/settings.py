273f33df6cfeca8b2f94b40e0aa9fd963c8af29b
YuvarajTana <yuvarajtana@gmail.com>
2023-04-25 07:53:29 +0530

    Add settings fiel

--- Django/myapp/myapp/settings.py
+++ Django/myapp/myapp/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x9_4*jm8b21z-g9i22vy6&d^r+z)5b^mlx-+urc57_qo6q5od)'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
