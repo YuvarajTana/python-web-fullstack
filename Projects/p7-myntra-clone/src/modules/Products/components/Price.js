ce6d81908794b4a6153054ddd1b5c6bfcc2e54fd
YuvarajTana <yuvarajtana@gmail.com>
2023-04-25 10:20:01 +0530

    Multiple pages

---
+++ Projects/p7-myntra-clone/src/modules/Products/components/Price.js
@@ -0,0 +1,9 @@
+import "../../../App.css";
+
+function Price(){
+  return <div className="Price">
+    <h5>PRICE</h5>
+  </div> 
+}
+
+export default Price;
---
+++ Projects/p7-myntra-clone/src/modules/Products/container/Products.js
@@ -0,0 +1,11 @@
+import Header from "../components/Header";
+import ProductsHome from '../components/ProductsHome';
+
+function Products() {
+  return <>
+    <Header></Header>
+    <ProductsHome></ProductsHome>
+  </>
+};
+
+export default Products;
